ee2561e7c028a1eb6e3e782c6c7c2771ab1ba9b0
Nathan <an.email.com>
2026-08-12 12:20:14 +0100

    simplify order request, allow all quantities to be known without market data

--- src/cex/order_request.rs
+++ src/cex/order_request.rs
@@ -1,4 +1,6 @@
-use crate::cex::orders::single_order::SingleOrder;
+use crate::cex::{
+    activation::Activation, quantity::Quantity, side::Side, tag::Tag, time_in_force::TimeInForce,
+};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 use strum::Display;
@@ -6,5 +8,28 @@ use strum::Display;
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Display, Clone, Hash)]
 pub enum OrderRequest<Asset, Number> {
-    Single(SingleOrder<Asset, Number>),
+    Limit {
+        base: Asset,
+        quote: Asset,
+        side: Side,
+        activation: Activation<Number>,
+        limit_price: Number,
+        time_in_force: TimeInForce,
+        quantity: Quantity<Number>,
+        tag: Tag,
+    },
+    MarketBuy {
+        base: Asset,
+        quote: Asset,
+        activation: Activation<Number>,
+        quote_quantity: Number,
+        tag: Tag,
+    },
+    MarketSell {
+        base: Asset,
+        quote: Asset,
+        activation: Activation<Number>,
+        base_quantity: Number,
+        tag: Tag,
+    },
 }
